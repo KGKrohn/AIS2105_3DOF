3b76028635fb7cf8cfe299a2efe160c4896de9e8
Maggebag <123036699+Maggebag@users.noreply.github.com>
2024-05-10 13:51:37 +0200

    Update balltracking.py
    updated to be more efficient and better

--- 3dof_camera/balltracking.py
+++ 3dof_camera/balltracking.py
@@ -12,12 +12,18 @@ class ballTrackingNode(Node):
     def __init__(self):
         super().__init__('ballTrackingNode')
 
+        # Create all subscribers and publishers
         self.subscription = self.create_subscription(
             Image,
             "image_raw",
             self.image_callback,
             10)
         self.subscription
+
+        self.publisher_mask = self.create_publisher(
+            Image,
+            'masked_image',
+            10)
 
         self.publisher_blur = self.create_publisher(
             Image,
@@ -49,25 +55,32 @@ class ballTrackingNode(Node):
         # Parameters
         self.declare_parameter('debug_mode', False)
 
-        self.declare_parameter('gaussian_blur_radius', 5)
+        self.declare_parameter('gaussian_blur_radius', 9)
 
-        self.declare_parameter('canny_lower_threshold', 190)
-        self.declare_parameter('canny_upper_threshold', 260)
+        self.declare_parameter('canny_lower_threshold', 20)
+        self.declare_parameter('canny_upper_threshold', 120)
 
         self.declare_parameter('hough_dp', 1.2)
         self.declare_parameter('hough_min_dist', 100)
         self.declare_parameter('hough_param1', 100)
         self.declare_parameter('hough_param2', 30)
-        self.declare_parameter('hough_min_radius', 5)
-        self.declare_parameter('hough_max_radius', 40)
-        self.declare_parameter('plate_hough_min_radius', 190)
-        self.declare_parameter('plate_hough_max_radius', 220)
+        self.declare_parameter('hough_min_radius', 21)
+        self.declare_parameter('hough_max_radius', 36)
 
     def image_callback(self, msg):
+        # Debug_mode to see all images in pipeline
         debug_mode = self.get_parameter('debug_mode').get_parameter_value().bool_value
 
         raw_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+
+        # Add a mask to the image we work with to remove undersired parts of image
+        mask = np.zeros_like(raw_image)
+
+        cv2.circle(mask, ((raw_image.shape[1] // 2)-5, (raw_image.shape[0] // 2)-1), 225, (255, 255, 255), thickness=cv2.FILLED)
+
+        masked_image = cv2.bitwise_and(raw_image, mask)
         
+        # Apply Gaussian blur
         blur_radius = self.get_parameter('gaussian_blur_radius').value
 
         # Ensure blur radius is odd
@@ -76,8 +89,9 @@ class ballTrackingNode(Node):
             self.set_parameters([rclpy.parameter.Parameter('gaussian_blur_radius', rclpy.Parameter.Type.INTEGER, blur_radius)])
             print("Please pass only odd values")
 
-        blurred_image = cv2.GaussianBlur(raw_image, (int(blur_radius), int(blur_radius)), 0)
+        blurred_image = cv2.GaussianBlur(masked_image, (int(blur_radius), int(blur_radius)), 0)
 
+        # Apply canny edge detection
         canny_lower_threshold = self.get_parameter('canny_lower_threshold').value
         canny_upper_threshold = self.get_parameter('canny_upper_threshold').value
 
@@ -90,40 +104,14 @@ class ballTrackingNode(Node):
         param2 = self.get_parameter('hough_param2').value
         min_radius = self.get_parameter('hough_min_radius').value
         max_radius = self.get_parameter('hough_max_radius').value
-        plate_min_radius = self.get_parameter('plate_hough_min_radius').value
-        plate_max_radius = self.get_parameter('plate_hough_max_radius').value
 
         hough_image = np.copy(raw_image)
 
-        plate = cv2.HoughCircles(edged_image, cv2.HOUGH_GRADIENT, dp, min_dist, param1=param1, param2=param2, minRadius=plate_min_radius, maxRadius=plate_max_radius)
-        
-        if plate is not None:
-            i = plate[0, 0]
-            if debug_mode:
-                # Draw the outer circle
-                cv2.circle(hough_image, (int(i[0]), int(i[1])), int(i[2]), (0, 255, 0), 2)
-                # Draw the center of the circle
-                cv2.circle(hough_image, (int(i[0]), int(i[1])), 2, (0, 0, 255), 3)
+        plate_center_msg = Float32MultiArray()
+        plate_center_msg.data = [float((raw_image.shape[1] // 2)-5), float((raw_image.shape[0] // 2)-1)]
+        self.publisher_plate_center_pos.publish(plate_center_msg)
 
-            # Radius of the circular mask
-            radius = int(plate[0, 0, 2] + 20)
-
-            # Circular mask
-            mask_plat = np.zeros(edged_image.shape, dtype='uint8')
-            cv2.circle(mask_plat, (int(plate[0, 0, 0]), int(plate[0, 0, 1])), radius, (255, 255, 255), -1)
-
-            plate_center_msg = Float32MultiArray()
-            plate_center_msg.data = [float(i[0]), float(i[1])]
-            self.publisher_plate_center_pos.publish(plate_center_msg)
-
-        else:
-            # Create an empty mask
-            mask_plat = np.zeros(edged_image.shape, dtype='uint8')
-
-        # Apply mask
-        masked_edged_image = cv2.bitwise_and(edged_image, edged_image, mask=mask_plat)
-
-        circles = cv2.HoughCircles(masked_edged_image, cv2.HOUGH_GRADIENT, dp, min_dist, param1=param1, param2=param2, minRadius=min_radius, maxRadius=max_radius)
+        circles = cv2.HoughCircles(edged_image, cv2.HOUGH_GRADIENT, dp, min_dist, param1=param1, param2=param2, minRadius=min_radius, maxRadius=max_radius)
 
         if circles is not None:
             i = circles[0, 0]
@@ -140,7 +128,11 @@ class ballTrackingNode(Node):
         ros2_hough_image_msg = self.bridge.cv2_to_imgmsg(hough_image, encoding='bgr8')
         self.publisher_hough.publish(ros2_hough_image_msg)
 
+        # Debug mode stuff
         if debug_mode:
+            ros2_masked_image_msg = self.bridge.cv2_to_imgmsg(masked_image, encoding='bgr8')
+            self.publisher_mask.publish(ros2_masked_image_msg)
+
             ros2_blurred_image_msg = self.bridge.cv2_to_imgmsg(blurred_image, encoding='bgr8')
             self.publisher_blur.publish(ros2_blurred_image_msg)
 
